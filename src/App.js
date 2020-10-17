b27a9b6531f3c5c4f3f6197f3355b5e5bdc490bb
Collin <collinhodgs@gmail.com>
2020-10-17 17:28:41 -0600

    Updated Route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar.js";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 import About from "./pages/About";

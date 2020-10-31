99ff886542fb37525c48372f882d011919d41896
Collin <collinhodgs@gmail.com>
2020-10-31 12:45:59 -0600

    Added more cards

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -166,6 +166,67 @@ const Projects = () => {
           </MDBCardBody>
         </MDBCard>
       </MDBCardGroup>
+      <MDBCardGroup deck className="mt-3">
+        <MDBCard>
+          <MDBCardImage
+            src="https://mdbootstrap.com/img/Photos/Others/images/16.jpg"
+            alt="MDBCard image cap"
+            top
+            hover
+            overlay="white-slight"
+          />
+          <MDBCardBody>
+            <MDBCardTitle tag="h5">Panel title</MDBCardTitle>
+            <MDBCardText>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </MDBCardText>
+            <MDBBtn color="light-blue" size="md">
+              read more
+            </MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+
+        <MDBCard>
+          <MDBCardImage
+            src="https://mdbootstrap.com/img/Photos/Others/images/14.jpg"
+            alt="MDBCard image cap"
+            top
+            hover
+            overlay="white-slight"
+          />
+          <MDBCardBody>
+            <MDBCardTitle tag="h5">Panel title</MDBCardTitle>
+            <MDBCardText>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </MDBCardText>
+            <MDBBtn color="light-blue" size="md">
+              read more
+            </MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+
+        <MDBCard>
+          <MDBCardImage
+            src="https://mdbootstrap.com/img/Photos/Others/images/15.jpg"
+            alt="MDBCard image cap"
+            top
+            hover
+            overlay="white-slight"
+          />
+          <MDBCardBody>
+            <MDBCardTitle tag="h5">Panel title</MDBCardTitle>
+            <MDBCardText>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </MDBCardText>
+            <MDBBtn color="light-blue" size="md">
+              read more
+            </MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCardGroup>
     </MDBContainer>
   );
 };

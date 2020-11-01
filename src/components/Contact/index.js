e00453a50b54a52e9974dc78a47553ba94a2ff11
Collin <collinhodgs@gmail.com>
2020-10-31 19:28:41 -0600

    Added actively looking for jobs description on contact page and added email contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -14,6 +14,18 @@ const ContactInfo = () => {
             class="pr-4 pb-3 img-fluid"
             alt="Collin Hodgson"
           />
+          <h4>
+            I recently gradated a Full Stack Web Development Bootcamp at the
+            University of Utah. I was given a certificate of completion from the
+            University. Now I am actively looking for a Junior Web Development
+            position, below I have linked my e-mail and cell phone number.
+            Please feel free to contact me at any time and if I do not
+            immediately respond I will respond within the next buisness day,
+            Thank you!
+          </h4>
+          <hr className="hr-light w-25" />
+          <h2>E-mail</h2>
+          <hr className="hr-dark w-25" />
         </MDBCol>
       </MDBRow>
     </MDBContainer>

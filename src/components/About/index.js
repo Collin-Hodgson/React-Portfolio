12e58d54d64490b1364e7d25ceada1dedfb6ab26
Collin <collinhodgs@gmail.com>
2020-11-13 23:41:28 -0700

    Changed font size in about me page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,18 +9,18 @@ const AboutMe = () => {
           <h2>About Me</h2>
           <hr class="solid bg-dark" />
           <img
-            style={{ height: "250px" }}
+            style={{ height: "285px" }}
             src="https://avatars0.githubusercontent.com/u/64516562?s=460&u=2778809b5be3b0ba187454d6c12564c0e67c369b&v=4"
             class="float-left pr-4 pb-3 img-fluid"
             alt="Collin Hodgson"
           />
-          <p>
+          <p style={{ fontSize: "30px" }}>
             My name is Collin Hodgson. I currently live in Ogden, UT. My hobbies
             are skateboarding, walking/hiking with my dogs, and golfing. I
             currently work at Hill AFB doing basic electrical work however I am
             taking the web development bootcamp at the University of Utah.
           </p>
-          <p>
+          <p style={{ fontSize: "30px" }}>
             I am detail orienteed with good verbal and written commuincation
             skills. I have good computer skills and am proficient with Microsoft
             Office. While I am working my current job I am enrolled in the web

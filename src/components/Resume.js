08d6b40a87a301b9955645128968e20e60934f05
Collin <collinhodgs@gmail.com>
2024-05-04 15:14:03 -0600

    update resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
-import Resume from "../assets/COLLIN_HODGSON_Resume.pdf";
+import Resume from "../assets/CHodgson_Resume.pdf";
 
 const AboutMe = () => {
   return (
